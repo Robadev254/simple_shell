073e7a01437a045d79646e22fd526d1ce02c08ad
Abigael chemtai <abigaelchemtai8@gmail.com>
2023-05-22 11:31:40 -0700

    bulilters

---
+++ main.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * main - function entry of Shell
+ * @ac: The argument counter
+ * @av: The argument values
+ * Return: if success = 0, exit
+*/
+
+int main(int ac __attribute__((unused)), char **av __attribute__((unused)))
+{
+	int int_mode = 1;
+	int errcode = 0;
+
+	errcode = errcode + 0;
+
+	int_mode = isatty(STDIN_FILENO);
+	if (int_mode == 0)
+	{
+		notty(av);
+	}
+	else
+	{
+
+		_interactive(av);
+	}
+	return (0);
+}
+/*** Antony Robert**/
+/*** Abigael chemtai */
